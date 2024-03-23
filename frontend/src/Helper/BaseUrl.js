ba80316627480ce46a9a5d03b634e33e1cee4fdb
Shilajit2002 <shilajit.acharjee@gmail.com>
2024-03-23 22:25:28 +0530

    Frontend Updated

--- frontend/src/Helper/BaseUrl.js
+++ frontend/src/Helper/BaseUrl.js
@@ -1,4 +1,5 @@
 const FRONTEND_SERVER = "http://localhost:3000"
-const BACKEND_SERVER = "http://localhost:8000/api/v1"
+// const BACKEND_SERVER = "http://localhost:8000/api/v1"
+const BACKEND_SERVER = "https://blogging-site-ca61.onrender.com/api/v1"
 
 export { FRONTEND_SERVER, BACKEND_SERVER };
