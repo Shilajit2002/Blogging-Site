5800453d7c7c25dff85b894d944ebcb1a2bdcd64
Shilajit2002 <shilajit.acharjee@gmail.com>
2024-03-23 22:35:19 +0530

    Frontend Updated

--- frontend/src/Helper/BaseUrl.js
+++ frontend/src/Helper/BaseUrl.js
@@ -1,4 +1,6 @@
-const FRONTEND_SERVER = "http://localhost:3000"
+// const FRONTEND_SERVER = "http://localhost:3000"
+const FRONTEND_SERVER = "https://blogmonk.netlify.app/"
+
 // const BACKEND_SERVER = "http://localhost:8000/api/v1"
 const BACKEND_SERVER = "https://blogging-site-ca61.onrender.com/api/v1"
 
